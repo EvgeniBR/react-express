f79561eb148a29ed040cc23c9a8c99b68c8cced4
EvgeniBR <genib1989@gmail.com>
2021-04-25 14:39:58 +0300

    a

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ app.get('/api/getUser', (req,res)=>{
     const user = 'Evgeni';
     res.json(user);
 })
-console.log(path.resolve(__dirname)+"\\client\\public\\index.html")
+
 const port = 8000;
-app.get('/', (request, response) => {
-    response.send(path.resolve(__dirname)+"\\client\\public\\index.html");
-  }).listen(process.env.PORT || port , () =>{
+app.listen(process.env.PORT || port , () =>{
     console.log(`Server started on port ${port}`)
 });
