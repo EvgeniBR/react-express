1fbf0a98d7380a225f3b6282bf4139e85ef0e25b
EvgeniBR <genib1989@gmail.com>
2021-04-25 14:30:07 +0300

    a

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 app.use(cors());
 app.get('/api/getUser', (req,res)=>{
     const user = 'Evgeni';
@@ -8,9 +9,8 @@ app.get('/api/getUser', (req,res)=>{
 })
 
 const port = 8000;
-
 app.get('/', (request, response) => {
-    response.send(response);
+    response.send(path.resolve(__dirname)+"\\clientpublic\\index.html");
   });
   app.listen(process.env.PORT || port , () =>{
     console.log(`Server started on port ${port}`)
