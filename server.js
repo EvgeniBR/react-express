39b9bac985ef26bb59919ee967a7ea616ed794d9
EvgeniBR <genib1989@gmail.com>
2021-04-25 15:30:13 +0300

    s

--- server.js
+++ server.js
@@ -9,8 +9,17 @@ app.get('/api/getUser', (req,res)=>{
 })
 
 const port = 8000;
-app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname,"/client/public/index.html"))
-  }).listen(process.env.PORT || port , () =>{
+
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+app.listen(process.env.PORT || port , () =>{
     console.log(`Server started on port ${port}`)
 });
